11ee5864607fb9aa9cc5fbc7c3fbe8481fa2cdfc
Josh Goldberg <josh@fullscreenmario.com>
2017-10-29 11:47:24 -0700

    Spaces!

--- src/StoreFactory.ts
+++ src/StoreFactory.ts
@@ -4,11 +4,11 @@ import { ChildStore } from "./ChildStore";
 import { TYPES } from "./Types";
 
 export class StoreFactory {
-	public constructor(
-		private readonly container: Container,
-	) { }
+    public constructor(
+        private readonly container: Container,
+    ) { }
 
-	public create(): ChildStore {
-		return this.container.get<() => ChildStore>(TYPES.ChildStore)();
-	}
+    public create(): ChildStore {
+        return this.container.get<() => ChildStore>(TYPES.ChildStore)();
+    }
 }
